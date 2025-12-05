c0e2091042d9a894d40e26c23d1327d5197379ae
ClaudiaQueipo <claudia.queipog@gmail.com>
2025-12-04 20:53:55 -0500

    fix(root): added automatic versioning, also removed unused icons in web docs

--- frontend-cli/index.js
+++ frontend-cli/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+import { readFileSync } from "fs"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
 import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
 
@@ -8,6 +11,13 @@ import * as generateCommand from "./cli/commands/generate.js"
 import * as initCommand from "./cli/commands/init.js"
 import * as moduleCommand from "./cli/commands/module.js"
 import * as uiLibraryCommand from "./cli/commands/ui-library.js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// Read version from root package.json
+const packageJson = JSON.parse(readFileSync(join(__dirname, "..", "package.json"), "utf-8"))
+const { version } = packageJson
 
 yargs(hideBin(process.argv))
   .scriptName("avangcli")
@@ -20,7 +30,7 @@ yargs(hideBin(process.argv))
   .demandCommand(1, "You need to specify a command")
   .help("h")
   .alias("h", "help")
-  .version("1.0.4")
+  .version(version)
   .alias("v", "version")
   .epilogue("For more information, visit: https://github.com/ClaudiaQueipo/avangcli")
   .parse()
